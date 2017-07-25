9567a40ca841f0ca686c387ef0536597249722ff
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-25 07:16:56 -0700

    add tableview with accessory types in cells

--- StylesAndAccessories/ViewController.swift
+++ StylesAndAccessories/ViewController.swift
@@ -14,12 +14,32 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    cell.textLabel?.text = "Accessorize Me!"
+    
+    switch indexPath.row {
+    case 0:
+      cell.accessoryType = .checkmark
+    case 1:
+      cell.accessoryType = .detailButton
+    case 2:
+      cell.accessoryType = .detailDisclosureButton
+    case 3:
+      cell.accessoryType = .disclosureIndicator
+    case 4:
+      cell.accessoryType = .none
+    default:
+      break
+    }
+    
+    return cell
+  }
+}
